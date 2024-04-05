05e2f835a3dcdfc4a027a28ea563d190b5cd6f94
Gerardo <74930791+ChopperNakama@users.noreply.github.com>
2024-04-05 13:35:10 -0500

    Add files via upload

---
+++ Dayan_Clase1.py
@@ -0,0 +1,10 @@
+#Crear un codigo que me permita ingresar 1 numero de 2 cifras y luego sumar sus cifras.
+Number= input("Escribe un número de dos dígitos\n")
+print(Number[0])
+print(Number[1])
+Number1= Number[0]
+Number2= Number[1]
+new_Number1= int(Number1)
+new_Number2= int(Number2)
+suma= new_Number1 + new_Number2
+print(suma)
